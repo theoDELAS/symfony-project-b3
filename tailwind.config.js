b98b29ec6f895d10bc09c1c48ab6edf4cc82deaf
Thomas Le Naour <lenaourthomas@gmail.com>
2021-01-05 11:41:45 +0100

    :lipstick: add Tailwind CSS and config files + base template

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enable: false,
+    content: ['./templates/*.twig.html', './templates/**/*.twig.html']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
