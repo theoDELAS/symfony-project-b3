ee38397242550f48c7c71545caa7bc662e6b3431
Théo <theo.delas@ynov.com>
2021-01-11 16:39:31 +0100

    :sparkles: profile page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,3 @@
 import '../css/app.css';
-import 'notyf/notyf.min.css'
+import 'notyf/notyf.min.css';
+import 'alpinejs';
---
+++ src/Controller/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function details(User $user = null): Response
+    {
+        $user = $user ?? $this->getUser();
+
+        if( !$user) {
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('user/details.html.twig', [
+            'user' => $user
+        ]);
+    }
+}
