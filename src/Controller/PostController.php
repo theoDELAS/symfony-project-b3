58cace56b669c589ddb8bf2b22ca6d8fbc45d522
Thomas Le Naour <lenaourthomas@gmail.com>
2021-01-11 16:43:12 +0100

    :construction: add post form and post controller

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -18,6 +22,32 @@ class PostController extends AbstractController
     {
         return $this->render('post/index.html.twig', [
             'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/post/new", name="post_create")
+     * @IsGranted("ROLE_USER")
+     */
+    public function create(Request $request, EntityManagerInterface $manager) {
+        $post = new Post();
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // ...
+
+            $manager->persist($post);
+            $manager->flush();
+
+            $this->addFlash('green', 'Publication créée');
+
+            return $this->redirectToRoute('post');
+        }
+
+        return $this->render('post/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/PostType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('picture')
+            ->add('location')
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'class' => 'form-input resize-none',
+                    'placeholder' => 'Adresse email'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
